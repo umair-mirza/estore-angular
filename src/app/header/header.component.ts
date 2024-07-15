58305762c20dd7a813c4560122356334dc326458
dreamygeek <umair_hp@hotmail.com>
2024-07-15 11:08:55 +0500

    added header and a basic search bar, also installed and used fontawesome

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  faSearch = faSearch;
+}
