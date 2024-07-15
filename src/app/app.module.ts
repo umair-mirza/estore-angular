0e7012d25c78fcd30b08a1416be1bb9eb7993280
dreamygeek <umair_hp@hotmail.com>
2024-07-15 12:42:56 +0500

    moved the header and navigation components to the home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { CatnavigationComponent } from './catnavigation/catnavigation.component';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, CatnavigationComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, HomeModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, HeaderComponent, CatnavigationComponent],
+  imports: [CommonModule, FontAwesomeModule],
 })
-export class HomeModule { }
+export class HomeModule {}
