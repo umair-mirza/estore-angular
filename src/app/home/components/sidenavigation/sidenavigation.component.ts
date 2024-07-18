47b8ccbb832f3861eae85d78dc22215611f178a5
dreamygeek <umair_hp@hotmail.com>
2024-07-18 20:12:42 +0500

    replaced get categories mock call with http get call

--- src/app/home/components/sidenavigation/sidenavigation.component.ts
+++ src/app/home/components/sidenavigation/sidenavigation.component.ts
@@ -12,12 +12,16 @@ export class SidenavigationComponent {
 
   //Inject the cateogry service in the constructor
   constructor(categoryService: CategoryService) {
-    this.categories = categoryService.getAllCategories();
+    categoryService.getAllCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
   }
 
   getCategories(parentCategoryId?: number): Category[] {
-    return this.categories?.filter(
-      (category) => category?.parent_category_id === parentCategoryId
+    return this.categories?.filter((category) =>
+      parentCategoryId
+        ? category?.parent_category_id === parentCategoryId
+        : category?.parent_category_id === null
     );
   }
 }
